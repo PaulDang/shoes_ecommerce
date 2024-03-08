ac71b9f7263eb21966e27301f6eb56f23f091ae5
bono43 <bono43@nexondv.com>
2024-03-08 12:51:44 +0700

    config lazy load and outlet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Suspense, lazy } from "react";
 import { useRoutes } from "react-router-dom";
 import "./App.css";
+import About from "./pages/about";
+import Contact from "./pages/contact";
 import Home from "./pages/home";
-import { Spinner } from "react-bootstrap";
+import Shop from "./pages/shop";
 
 const LayoutLazy = lazy(() => import("./pages/layout"));
 
@@ -14,7 +16,7 @@ function App() {
         <Suspense
           fallback={
             <div className="w-full flex justify-center items-center bg-white h-screen">
-              <Spinner animation="border" />
+              <div className="rounded-md h-12 w-12 border-4 border-t-4 border-blue-500 animate-spin absolute" />
             </div>
           }
         >
@@ -24,6 +26,9 @@ function App() {
       children: [
         { path: "/", element: <Home /> },
         { path: "/home", element: <Home /> },
+        { path: "/shop", element: <Shop /> },
+        { path: "/about", element: <About /> },
+        { path: "/contact", element: <Contact /> },
       ],
     },
     { path: "*", element: <>Error</> },
